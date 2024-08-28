947e91ca8b5015beec0c595f37b4f2a61bf1bcf9
gilbert sahyoun <gilbertsahyoun.business@gmail.com>
2024-08-28 12:14:15 +0300

    Finish About container

---
+++ index.js
@@ -0,0 +1,38 @@
+function isElementInViewport(element, threshold = 0.5) {
+  const rect = element.getBoundingClientRect();
+  const elementHeight = rect.bottom - rect.top;
+  const elementInView = Math.max(
+    0,
+    Math.min(rect.bottom, window.innerHeight) - Math.max(rect.top, 0)
+  );
+
+  return elementInView / elementHeight >= threshold;
+}
+
+function toggleImageVisibility(imageId, textId) {
+  const imageContainer = document.querySelector(`#${imageId}`);
+  const aboutText = document.querySelector(`#${textId}`);
+
+  if (isElementInViewport(aboutText, 0.2)) {
+    imageContainer.style.opacity = 1;
+    imageContainer.style.transform = "translateY(0)";
+  } else {
+    imageContainer.style.opacity = 0;
+    imageContainer.style.transform = "translateY(30px)";
+  }
+}
+
+window.addEventListener("scroll", function () {
+  toggleImageVisibility("image1", "text1");
+  toggleImageVisibility("image2", "text2");
+  toggleImageVisibility("image3", "text3");
+  toggleImageVisibility("image5", "text3");
+  toggleImageVisibility("image4", "text1");
+});
+
+// Initial check
+toggleImageVisibility("image1", "text1");
+toggleImageVisibility("image2", "text2");
+toggleImageVisibility("image3", "text3");
+toggleImageVisibility("image5", "text3");
+toggleImageVisibility("image4", "text1");
